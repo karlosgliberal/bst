3fd75b00c8ea72ee9b5eab617d9186234d2bca60
David Gil <david.gil@biko2.com>
2014-07-01 11:30:15 +0200

    Cambio formato de tag php en deepbanner

--- sites/all/themes/bestaker/templates/views/slides_home/views-view-fields--slide-principal--panel-pane-1.tpl.php
+++ sites/all/themes/bestaker/templates/views/slides_home/views-view-fields--slide-principal--panel-pane-1.tpl.php
@@ -26,13 +26,13 @@
 // dpm($row);
 ?>
 
-<div class="slide-home" style="background-image:url(/sites/default/files/<? print $row->field_field_slide_imagen[0]['rendered']['#item']['filename']; ?>);">
+<div class="slide-home" style="background-image:url(/sites/default/files/<?php print $row->field_field_slide_imagen[0]['rendered']['#item']['filename']; ?>);">
   <div class="container">
       <div class="row">
         <div class="col-md-12 center inner-65">
-          <h2 class="slide-big shadow"><? print $row->node_title ?></h2>
+          <h2 class="slide-big shadow"><?php print $row->node_title ?></h2>
            <div class="slide-texto shadow mt30">
-            <? print $row->field_field_slide_descripcion[0]['rendered']['#markup']; ?>
+            <?php print $row->field_field_slide_descripcion[0]['rendered']['#markup']; ?>
           </div>
         </div>
       </div>
@@ -43,10 +43,10 @@
         <div class="row">
           <div class="col-md-12" >
             <div class=" col-md-8 texto-apuntate">
-              <? print $row->field_field_slide_descripcion_apuntate[0]['rendered']['#markup']; ?>
+              <?php print $row->field_field_slide_descripcion_apuntate[0]['rendered']['#markup']; ?>
             </div>
             <div class="col-md-4 boton-apuntate center mt10">
-              <a class="btn btn-blue btn-large" href="<? print $row->field_field_slide_enlace_apuntate[0]['raw']['url']; ?>"><? print $row->field_field_slide_enlace_apuntate[0]['raw']['title']; ?></a>
+              <a class="btn btn-blue btn-large" href="<?php print $row->field_field_slide_enlace_apuntate[0]['raw']['url']; ?>"><?php print $row->field_field_slide_enlace_apuntate[0]['raw']['title']; ?></a>
             </div>
           </div>
         </div>
@@ -59,10 +59,10 @@
         <div class="row">
           <div class="col-md-12" >
             <div class="col-sm-8 col-md-8 texto-apuntate shadow">
-              <? print $row->field_field_slide_descripcion_noticia[0]['rendered']['#markup']; ?>
+              <?php print $row->field_field_slide_descripcion_noticia[0]['rendered']['#markup']; ?>
             </div>
             <div class="col-sm-4 col-md-4 boton-apuntate center mt10">
-              <a class="btn btn-blue btn-large" href="<? print $row->field_field_slide_enlace_noticia[0]['raw']['url']; ?>"><?print $row->field_field_slide_enlace_noticia[0]['raw']['title']; ?></a>
+              <a class="btn btn-blue btn-large" href="<?php print $row->field_field_slide_enlace_noticia[0]['raw']['url']; ?>"><?print $row->field_field_slide_enlace_noticia[0]['raw']['title']; ?></a>
             </div>
           </div>
         </div>
