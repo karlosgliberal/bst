ea833404df499d0fb598692aab46137897bad823
karlos g liberal <karlosgliberal@gmail.com>
2014-08-08 10:48:43 +0200

    Añadida el control del campo gráfico para evitar que este vacío envíe cosas innecesarias

--- sites/all/modules/bestaker_features/sociedades/sociedades.graficos.js
+++ sites/all/modules/bestaker_features/sociedades/sociedades.graficos.js
@@ -1,30 +1,31 @@
 (function ($) {
   Drupal.behaviors.sociedades = {
     attach: function(context, settings) {
-      console.log(settings);
-      google.load('visualization', '1', {'callback':function(){drawChart();}, 'packages':['corechart']});
-      //google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var query = new google.visualization.Query(settings.sociedad_grafico_capital);
-        query.send(handleQueryResponse);
-      }
-
-      function handleQueryResponse(response) {
-        if (response.isError()) {
-          alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-          return;
+      if(settings.sociedad_grafico_capital !== undefined){
+        google.load('visualization', '1', {'callback':function(){drawChart();}, 'packages':['corechart']});
+        //google.setOnLoadCallback(drawChart);
+        function drawChart() {
+          var query = new google.visualization.Query(settings.sociedad_grafico_capital);
+          query.send(handleQueryResponse);
         }
-        var data = response.getDataTable();
-        var options = {
-          width: 500,
-          height: 450,
-          legend: { position: 'top', maxLines: 2 },
-          bar: { groupWidth: '75%' },
-          isStacked: true,
-        };
 
-        var chart = new google.visualization.ColumnChart(document.getElementById('columnchart'));
-        chart.draw(data, options);
+        function handleQueryResponse(response) {
+          if (response.isError()) {
+            alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+            return;
+          }
+          var data = response.getDataTable();
+          var options = {
+            width: 500,
+            height: 450,
+            legend: { position: 'top', maxLines: 2 },
+            bar: { groupWidth: '75%' },
+            isStacked: true,
+          };
+
+          var chart = new google.visualization.ColumnChart(document.getElementById('columnchart'));
+          chart.draw(data, options);
+        }
       }
     }
   };
